c11568f30123268f0b9e06292884ccee7ba0a7bd
Edi Santoso <edicyber@gmail.com>
2016-10-05 11:47:27 +0700

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,20 +3,13 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Dashboard</title>
+  <title>Admin Person Page | Masiya Assessments</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- jvectormap -->
-  <!-- Theme style -->
   <link rel="stylesheet" href="css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="css/skin-blue-light.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -29,7 +22,6 @@
 <body class="hold-transition skin-blue-light sidebar-mini">
       <div class="row">
         <div class="col-md-12">
-          <!-- Custom Tabs (Pulled to the right) -->
           <div class="nav-tabs-custom">
             <ul class="nav nav-tabs pull-right" >
               <li><a href="#department" data-toggle="tab">Department</a></li>
@@ -97,11 +89,7 @@
         <!-- /.col -->
       </div>
       <!-- /.row -->
-<!-- ./wrapper -->
-
-<!-- jQuery 2.2.3 -->
 <script src="js/jquery-2.2.3.min.js"></script>
-<!-- Bootstrap 3.3.6 -->
 <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
